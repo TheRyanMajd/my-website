3830a1b29275e85bacb4a5c33f2e3eeb8a94573a
TheRyanMajd <131102091+TheRyanMajd@users.noreply.github.com>
2023-05-10 14:38:14 -0400

    changed js toggle mode funciton for project tab

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function toggleMode() {
   const hero = document.querySelector(".hero");
   const navLinks = document.querySelectorAll("nav a");
   const button = document.querySelector("button");
+  const columnLeft = document.querySelector(".column-left");
 
   if (button.textContent === "Light Mode") {
     body.style.backgroundColor = "#fff";
@@ -13,15 +14,17 @@ function toggleMode() {
     hero.style.color = "black";
     button.style.backgroundColor = "#BA0C2F";
     button.style.color = "#fff";
+    columnLeft.style.color = "black";
     button.textContent = "Dark Mode";
   } else {
-    body.style.backgroundColor = "";
+    body.style.backgroundColor = "000";
     nav.style.backgroundColor = "#BA0C2F";
-    navLinks.forEach((link) => (link.style.color = ""));
+    navLinks.forEach((link) => (link.style.color = "black"));
     hero.style.backgroundColor = "black";
     hero.style.color = "#fff";
     button.style.backgroundColor = "";
     button.style.color = "";
+    columnLeft.style.color = "white";
     button.textContent = "Light Mode";
   }
 }
