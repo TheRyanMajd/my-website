584254f5da65a3820b3f8732919487b80a76d557
TheRyanMajd <131102091+TheRyanMajd@users.noreply.github.com>
2023-05-10 00:21:59 -0400

    Added files via upload from offline repo

---
+++ app.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const body = document.querySelector("body");
+  const nav = document.querySelector("nav");
+  const hero = document.querySelector(".hero");
+  const navLinks = document.querySelectorAll("nav a");
+  const button = document.querySelector("button");
+
+  if (button.textContent === "Light Mode") {
+    body.style.backgroundColor = "#fff";
+    nav.style.backgroundColor = "#BA0C2F";
+    navLinks.forEach((link) => (link.style.color = "#fff"));
+    hero.style.backgroundColor = "#fff";
+    button.textContent = "Dark Mode";
+	document.querySelector(".column-left").style.color = "black";
+    hero.style.color = "black";
+
+  } else {
+    body.style.backgroundColor = "";
+    nav.style.backgroundColor = "#BA0C2F";
+    navLinks.forEach((link) => (link.style.color = ""));
+    hero.style.backgroundColor = "black";
+	document.querySelector(".column-left").style.color = "white";
+    button.textContent = "Light Mode";
+  }
+}
