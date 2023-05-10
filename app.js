dc5aaa8f6ae0cce11917bb93a27cb04428c729ee
TheRyanMajd <131102091+TheRyanMajd@users.noreply.github.com>
2023-05-10 03:13:01 -0400

    js changes for toggle button

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-const projectPage = document.querySelector(".project-page");
-const projects = document.querySelectorAll(".project");
+function toggleMode() {
+  const body = document.querySelector("body");
+  const nav = document.querySelector("nav");
+  const hero = document.querySelector(".hero");
+  const navLinks = document.querySelectorAll("nav a");
+  const button = document.querySelector("button");
+  const project = document.querySelectorAll(".project");
 
-if (button.textContent === "Light Mode") {
-  // Light mode styles
-  body.style.backgroundColor = "#fff";
-  nav.style.backgroundColor = "#BA0C2F";
-  navLinks.forEach((link) => (link.style.color = "#fff"));
-  hero.style.backgroundColor = "#fff";
-  button.textContent = "Dark Mode";
-  document.querySelector(".column-left").style.color = "black";
-  hero.style.color = "black";
-  projectPage.style.backgroundColor = "#fff";
-  projects.forEach((project) => {
-    project.style.backgroundColor = "#fff";
-  });
-} else {
-  // Dark mode styles
-  body.style.backgroundColor = "";
-  nav.style.backgroundColor = "#BA0C2F";
-  navLinks.forEach((link) => (link.style.color = ""));
-  hero.style.backgroundColor = "black";
-  document.querySelector(".column-left").style.color = "white";
-  button.textContent = "Light Mode";
-  hero.style.color = "white";
-  projectPage.style.backgroundColor = "black";
-  projects.forEach((project) => {
-    project.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
-  });
+  if (button.textContent === "Light Mode") {
+    body.style.backgroundColor = "#fff";
+    nav.style.backgroundColor = "#BA0C2F";
+    navLinks.forEach((link) => (link.style.color = "#fff"));
+    hero.style.backgroundColor = "#fff";
+    button.textContent = "Dark Mode";
+    document.querySelector(".column-left").style.color = "black";
+    hero.style.color = "black";
+    project.forEach((project) => {
+      project.style.backgroundColor = "#eee";
+      project.style.color = "black";
+    });
+
+  } else {
+    body.style.backgroundColor = "";
+    nav.style.backgroundColor = "#BA0C2F";
+    navLinks.forEach((link) => (link.style.color = ""));
+    hero.style.backgroundColor = "black";
+    document.querySelector(".column-left").style.color = "white";
+    button.textContent = "Light Mode";
+    project.forEach((project) => {
+      project.style.backgroundColor = "";
+      project.style.color = "";
+    });
+  }
 }
