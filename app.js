26d9b98daac5b3fff5389ec5a462f39f5957386f
TheRyanMajd <131102091+TheRyanMajd@users.noreply.github.com>
2023-05-10 03:07:55 -0400

    trying to make the dark and light mode work on project page and css styling

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-function toggleMode() {
-  const body = document.querySelector("body");
-  const nav = document.querySelector("nav");
-  const hero = document.querySelector(".hero");
-  const navLinks = document.querySelectorAll("nav a");
-  const button = document.querySelector("button");
+const projectPage = document.querySelector(".project-page");
+const projects = document.querySelectorAll(".project");
 
-  if (button.textContent === "Light Mode") {
-    body.style.backgroundColor = "#fff";
-    nav.style.backgroundColor = "#BA0C2F";
-    navLinks.forEach((link) => (link.style.color = "#fff"));
-    hero.style.backgroundColor = "#fff";
-    button.textContent = "Dark Mode";
-	document.querySelector(".column-left").style.color = "black";
-    hero.style.color = "black";
-
-  } else {
-    body.style.backgroundColor = "";
-    nav.style.backgroundColor = "#BA0C2F";
-    navLinks.forEach((link) => (link.style.color = ""));
-    hero.style.backgroundColor = "black";
-	document.querySelector(".column-left").style.color = "white";
-    button.textContent = "Light Mode";
-  }
+if (button.textContent === "Light Mode") {
+  // Light mode styles
+  body.style.backgroundColor = "#fff";
+  nav.style.backgroundColor = "#BA0C2F";
+  navLinks.forEach((link) => (link.style.color = "#fff"));
+  hero.style.backgroundColor = "#fff";
+  button.textContent = "Dark Mode";
+  document.querySelector(".column-left").style.color = "black";
+  hero.style.color = "black";
+  projectPage.style.backgroundColor = "#fff";
+  projects.forEach((project) => {
+    project.style.backgroundColor = "#fff";
+  });
+} else {
+  // Dark mode styles
+  body.style.backgroundColor = "";
+  nav.style.backgroundColor = "#BA0C2F";
+  navLinks.forEach((link) => (link.style.color = ""));
+  hero.style.backgroundColor = "black";
+  document.querySelector(".column-left").style.color = "white";
+  button.textContent = "Light Mode";
+  hero.style.color = "white";
+  projectPage.style.backgroundColor = "black";
+  projects.forEach((project) => {
+    project.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+  });
 }
