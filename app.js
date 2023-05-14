446b979e6d0c5e762b1a90bfe050b17790e894be
TheRyanMajd <rm97798@uga.edu>
2023-05-13 20:09:24 -0400

    Added mobile support

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
-function toggleMode() {
-  const body = document.querySelector("body");
-  const nav = document.querySelector("nav");
-  const hero = document.querySelector(".hero");
-  const navLinks = document.querySelectorAll("nav a");
-  const button = document.querySelector("button");
-  const columnLeft = document.querySelector(".column-left");
-  const columnRight = document.querySelector(".column-right");
+window.onload = function() {
+  checkMobileCSS();
+};
 
-  if (button.textContent === "Light Mode") {
-    body.style.backgroundColor = "#fff";
-    nav.style.backgroundColor = "#BA0C2F";
-    navLinks.forEach((link) => (link.style.color = "#fff"));
-    hero.style.backgroundColor = "#fff";
-    hero.style.color = "black";
-    button.style.backgroundColor = "#BA0C2F";
-    button.style.color = "#fff";
-    columnLeft.style.color = "black";
-    columnRight.style.color = "black";
-    button.textContent = "Dark Mode";
-  } else {
-    body.style.backgroundColor = "000";
-    nav.style.backgroundColor = "#BA0C2F";
-    navLinks.forEach((link) => (link.style.color = "black"));
-    hero.style.backgroundColor = "black";
-    hero.style.color = "#fff";
-    button.style.backgroundColor = "";
-    button.style.color = "";
-    columnLeft.style.color = "white";
-    columnRight.style.color = "white";
-    button.textContent = "Light Mode";
-  }
+function checkMobileCSS() {
+  
+    if (window.matchMedia("(max-width: 767px)").matches) {
+ 
+      const columnLeft = document.querySelector('.column-left');
+      const image = columnLeft.querySelector('img');
+      image.src = './Resources/Images/mobile.gif';
+
+    } 
 }
