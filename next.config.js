a4953f3f6a68444f850130a8f7909438f8ce4797
TheRyanMajd <rm97798@uga.edu>
2025-05-15 13:35:11 -0400

    chore: update next.js to version 15.3.2 in package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
     output: 'export',
+    images: {
+        unoptimized: false, // change to false when deploying
+    }
     }
     
     module.exports = nextConfig
